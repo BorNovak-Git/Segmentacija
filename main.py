885d873e0eeb98029b48955d12d5905970f08d43
bornovak <novakbor999@gmail.com>
2025-05-03 14:20:39 +0200

    SEG-1 Added function to calculate centers

--- main.py
+++ main.py
@@ -1,6 +1,62 @@
 import cv2 as cv
 import numpy as np
 
+def izracunaj_centre(slika, izbira, dimenzija_centra, T, k):
+    h, w, c = slika.shape
+    centri = []
+
+    if izbira == "nakljucno":
+        while len(centri) < k:
+            x = np.random.randint(0, w)
+            y = np.random.randint(0, h)
+            barva = slika[y, x]
+
+            if dimenzija_centra == 5:
+                kandidat = np.array([barva[0], barva[1], barva[2], x, y])
+            else:
+                kandidat = np.array([barva[0], barva[1], barva[2]])
+
+            preblizu = False
+            for c in centri:
+                if np.linalg.norm(c[:3] - kandidat[:3]) < T:
+                    preblizu = True
+                    break
+
+            if not preblizu:
+                centri.append(kandidat)
+
+    elif izbira == "rocno":
+        kliknjeni_centri = []
+
+        def klik_mis(event, x, y, flags, param):
+            if event == cv.EVENT_LBUTTONDOWN:
+                kliknjeni_centri.append((x, y))
+                print(f"Kliknil: ({x},{y})")
+
+        cv.namedWindow("Klikni centre", cv.WINDOW_NORMAL)
+        cv.imshow("Klikni centre", slika)
+        cv.setMouseCallback("Klikni centre", klik_mis)
+
+        print(f"Klikni {k} centrov z miško...")
+        while len(kliknjeni_centri) < k:
+            cv.waitKey(1)
+
+        cv.destroyWindow("Klikni centre")
+
+        for (x, y) in kliknjeni_centri:
+            barva = slika[y, x]
+            if dimenzija_centra == 5:
+                kandidat = np.array([barva[0], barva[1], barva[2], x, y])
+            else:
+                kandidat = np.array([barva[0], barva[1], barva[2]])
+            centri.append(kandidat)
+
+    else:
+        raise ValueError("Napaka: izbira mora biti 'nakljucno' ali 'rocno'.")
+
+    centri = np.array(centri, dtype=np.float32)
+    return centri
+
 
 if __name__ == "__main__":
     """
